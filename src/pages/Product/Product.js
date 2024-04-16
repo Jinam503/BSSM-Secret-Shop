339675b62eefead094f80c781aeaa2d5b3be37e9
백진암 <jinam1234567@gmail.com>
2024-04-16 22:23:23 +0900

    FIX: 파일 구조야야야야양

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useProducts } from "../../components/ProductsContext";
-import { useProductPage } from "./hooks/ProductPageHook";
+import axios from "axios";
+import { notify } from "../../components/Toast";
 import * as B from "../../styles/BaseStructueStyle";
 import * as S from "./style/ProductPageStyle";
 
@@ -9,15 +10,62 @@ const Product = () => {
   const { products, setProducts } = useProducts();
   const [items, setItems] = useState([]);
   const [category, setCategory] = useState("전체");
-  const { fetchProducts, AddProductToCart } = useProductPage(
-    setItems,
-    products,
-    setProducts
-  );
 
   useEffect(() => {
     fetchProducts();
   }, []);
+  const fetchProducts = async () => {
+    try {
+      const response = await axios
+        .get(process.env.REACT_APP_SERVER_URL + "api/products")
+        .then((res) => {
+          setItems(res.data);
+        });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      notify({
+        type: "error",
+        text: "제품 정보를 가져오는데 실패했습니다.",
+      });
+    }
+  };
+
+  const AddProductToCart = (item) => {
+    const index = products.findIndex((product) => product.id === item.id);
+
+    if (index !== -1) {
+      const updatedProducts = [...products];
+      if (updatedProducts[index].amount < updatedProducts[index].maxStock) {
+        updatedProducts[index].amount++;
+        setProducts(updatedProducts);
+        notify({
+          type: "success",
+          text: "장바구니에 " + item.name + "를 추가했습니다.",
+        });
+      } else
+        notify({
+          type: "error",
+          text: "재고가 부족합니다.",
+        });
+    } else {
+      const newItem = {
+        id: item.id,
+        name: item.name,
+        checked: true,
+        category: item.category,
+        description: item.description,
+        url: item.imageUrl,
+        price: item.price,
+        amount: 1,
+        maxStock: item.stock,
+      };
+      setProducts([...products, newItem]);
+      notify({
+        type: "success",
+        text: "장바구니에 " + item.name + "를 추가했습니다.",
+      });
+    }
+  };
   return (
     <div>
       <B.BodyContainer>
