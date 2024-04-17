eb1e9aa40fe591563fd643a77f358f8cfc80c4f4
백진암 <jinam1234567@gmail.com>
2024-04-17 11:52:46 +0900

    FIX: 상품추가제한 진짜

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -57,22 +57,29 @@ const Product = () => {
         });
       }
     } else {
-      const newItem = {
-        id: item.id,
-        name: item.name,
-        checked: true,
-        category: item.category,
-        description: item.description,
-        url: item.imageUrl,
-        price: item.price,
-        amount: 1,
-        maxStock: item.stock,
-      };
-      setProducts([...products, newItem]);
-      notify({
-        type: "success",
-        text: "장바구니에 " + item.name + "를 추가했습니다.",
-      });
+      if (totalAmount >= 10) {
+        notify({
+          type: "error",
+          text: "상품은 최대 10까지 담을 수 있습니다.",
+        });
+      } else {
+        const newItem = {
+          id: item.id,
+          name: item.name,
+          checked: true,
+          category: item.category,
+          description: item.description,
+          url: item.imageUrl,
+          price: item.price,
+          amount: 1,
+          maxStock: item.stock,
+        };
+        setProducts([...products, newItem]);
+        notify({
+          type: "success",
+          text: "장바구니에 " + item.name + "를 추가했습니다.",
+        });
+      }
     }
   };
   return (
